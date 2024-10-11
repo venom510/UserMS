d687638c3aedef55413b074e6de99ec00c70b6f5
Manav Upadhyay <manavupadhyay5@gmail.com>
2024-10-12 01:54:12 +0530

    Updated root package name

--- user-ms/src/main/java/com/hoteladvisor/user/UserMsApplication.java
+++ user-ms/src/main/java/com/hoteladvisor/user/UserMsApplication.java
@@ -1,4 +1,4 @@
-package user_ms;
+package com.hoteladvisor.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- user-ms/src/test/java/com/hoteladvisor/user/UserMsApplicationTests.java
+++ user-ms/src/test/java/com/hoteladvisor/user/UserMsApplicationTests.java
@@ -1,4 +1,4 @@
-package user_ms;
+package com.hoteladvisor.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
